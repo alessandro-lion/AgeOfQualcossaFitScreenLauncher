eed852729800939e314aa026b1a9c5621c152673
alessandro.lion@outlook.com <mondeo93>
2018-06-23 22:01:27 +0200

    FIX
    Resolved Issue 1

--- AgeOfQualcossaFitScreenLauncher/Form1.Designer.cs
+++ AgeOfQualcossaFitScreenLauncher/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
@@ -80,6 +81,7 @@
             this.ClientSize = new System.Drawing.Size(480, 320);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.labelres);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Age Of Qualcossa Fit Screen Launcher - Powered by P. Lion to make children happy";
             this.Load += new System.EventHandler(this.Form1_Load);
--- AgeOfQualcossaFitScreenLauncher/Form1.cs
+++ AgeOfQualcossaFitScreenLauncher/Form1.cs
@@ -10,12 +10,15 @@ namespace AgeOfQualcossaFitScreenLauncher
     public partial class Form1 : Form
     {
         private string AOQresparm = "";
+        private string AO2resparm = "";
         private static string AOMX = "C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\aomx.exe";
         private static string AOM = "C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\aom.exe";
         private static string AOE2Conquer = "C:\\Program Files (x86)\\Microsoft Games\\Age of Empires II\\Age2_X1\\age2_x1.Exe";
         private static string AOE2 = "C:\\Program Files (x86)\\Microsoft Games\\Age of Empires II\\EMPIRES2.EXE";
         private StringCollection gamepath = new StringCollection();
-
+        private StringCollection gamearg = new StringCollection();
+        private int dx;
+        private int dy;
         public Form1()
         {
             InitializeComponent();
@@ -23,18 +26,29 @@ namespace AgeOfQualcossaFitScreenLauncher
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+             dx = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+             dy = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
             // Checking screen resolution and set AOQresparm
-            AOQresparm = " xres=" + System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width.ToString()+ " yres="+ System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height.ToString();
+            AOQresparm = " xres=" + dx.ToString()+ " yres="+ dy.ToString();
             labelres.Text = "SIZE:" + AOQresparm;
+            if ((dx > 1279) && (dy > 1023))
+            { AO2resparm = " 1280"; }
+            else if ((dx > 1023) && (dy > 767))
+            {    AO2resparm = " 1024";   }
+            else if ((dx > 799) && (dy > 599))
+            // 800x600
+            // 1024x768
+            // 1280x1024
+            { AO2resparm = " 800"; }
+
             // Inspecting filesystem
-            CheckAOGame("Age of Mythology", AOM);
-            CheckAOGame("Age of Mythology Titan Expansion", AOMX);
-            CheckAOGame("Age of Empires II", AOE2);
-            CheckAOGame("Age of Empires II The Conquer", AOE2Conquer);
+            CheckAOGame("Age of Mythology", AOM, AOQresparm);
+            CheckAOGame("Age of Mythology Titan Expansion", AOMX, AOQresparm);
+            CheckAOGame("Age of Empires II", AOE2, AO2resparm);
+            CheckAOGame("Age of Empires II The Conquer", AOE2Conquer, AO2resparm);
 
         }
-        private void CheckAOGame(string Title, string Path)
+        private void CheckAOGame(string Title, string Path, string Arg)
         {
             //If the game exist I add it to the Listbox on the form
             try
@@ -45,6 +59,7 @@ namespace AgeOfQualcossaFitScreenLauncher
             {
                 listBox1.Items.Add(Title);
                 gamepath.Add(Path);
+                gamearg.Add(Arg);
             }
             }
             catch (Exception exception)
@@ -60,9 +75,9 @@ namespace AgeOfQualcossaFitScreenLauncher
                 {
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.FileName = gamepath[listBox1.SelectedIndex];
+                startInfo.WorkingDirectory = new FileInfo(gamepath[listBox1.SelectedIndex]).Directory.FullName;
 
-
-                startInfo.Arguments = AOQresparm;
+                startInfo.Arguments = gamearg[listBox1.SelectedIndex];
                 Process.Start(startInfo);
             }
             else
