38f67e73103674c9e146f5994907adac26493f2c
alessandro.lion@outlook.com <mondeo93>
2018-06-23 20:00:40 +0200

    Initial Load
    Release 1.0

---
+++ AgeOfQualcossaFitScreenLauncher/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AgeOfQualcossaFitScreenLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelres = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "boton che fa partire el zogo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 286);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seesiona soea lista e schicia el boton";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(387, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // labelres
+            // 
+            this.labelres.AutoSize = true;
+            this.labelres.Location = new System.Drawing.Point(31, 301);
+            this.labelres.Name = "labelres";
+            this.labelres.Size = new System.Drawing.Size(35, 13);
+            this.labelres.TabIndex = 1;
+            this.labelres.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 320);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelres);
+            this.Name = "Form1";
+            this.Text = "Age Of Qualcossa Fit Screen Launcher - Powered by P. Lion to make children happy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelres;
+    }
+}
+
---
+++ AgeOfQualcossaFitScreenLauncher/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AgeOfQualcossaFitScreenLauncher
+{
+    public partial class Form1 : Form
+    {
+        private string AOQresparm = "";
+        private static string AOMX = "C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\aomx.exe";
+        private static string AOM = "C:\\Program Files (x86)\\Microsoft Games\\Age of Mythology\\aom.exe";
+        private static string AOE2Conquer = "C:\\Program Files (x86)\\Microsoft Games\\Age of Empires II\\Age2_X1\\age2_x1.Exe";
+        private static string AOE2 = "C:\\Program Files (x86)\\Microsoft Games\\Age of Empires II\\EMPIRES2.EXE";
+        private StringCollection gamepath = new StringCollection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Checking screen resolution and set AOQresparm
+            AOQresparm = " xres=" + System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width.ToString()+ " yres="+ System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height.ToString();
+            labelres.Text = "SIZE:" + AOQresparm;
+            // Inspecting filesystem
+            CheckAOGame("Age of Mythology", AOM);
+            CheckAOGame("Age of Mythology Titan Expansion", AOMX);
+            CheckAOGame("Age of Empires II", AOE2);
+            CheckAOGame("Age of Empires II The Conquer", AOE2Conquer);
+
+        }
+        private void CheckAOGame(string Title, string Path)
+        {
+            //If the game exist I add it to the Listbox on the form
+            try
+
+            { 
+            FileInfo sFile = new FileInfo(AOMX);
+            if (sFile.Exists)
+            {
+                listBox1.Items.Add(Title);
+                gamepath.Add(Path);
+            }
+            }
+            catch (Exception exception)
+
+             {
+                System.Diagnostics.Debug.Print(exception.Message + " but I don't care \n");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >0)
+                {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = gamepath[listBox1.SelectedIndex];
+
+
+                startInfo.Arguments = AOQresparm;
+                Process.Start(startInfo);
+            }
+            else
+            {
+                labelres.Text = "Seesiona un zugo soea lista!";
+            }
+        }
+    }
+}
